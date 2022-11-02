06259d335c4b8190d48745572cbf6c43d8669e29
moncef <98907255+moncxxf@users.noreply.github.com>
2022-11-02 16:07:38 +0100

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,18 @@
+const FooterMessage = "Betty. Simple ting";
+const code = [
+    {
+        'code':'moncef',
+        'moins':'300DA'
+    }
+]
+const Products = [
+    {
+        'brand':'Corteiz',
+        'name':'SkyBlue Trousers',
+        'price':'6500 DA',
+        'thumb':'https://cdn.shopify.com/s/files/1/0476/8301/0716/products/DC5053F1-5596-4629-B186-F617A494AFC4_1_201_a.heic',
+        'img':'https://i.imgur.com/ZG7MEaB.png',
+        'link':'products/SkyBlue Trousers.html',
+        'available':'yes'
+    }
+]
---
+++ assets/main.js
@@ -0,0 +1,68 @@
+window.onload = function () {
+    document.querySelector('#footerMessage').innerHTML = FooterMessage
+    LoadProducts();
+    change()
+    Shop();
+    promcode();
+}
+function order(){
+  var numero = document.querySelector('#numero')
+  if(numero.value == ""){
+    Swal.fire(
+      'Confirm Your Number',
+      'We cant contact you without your number',
+      'question'
+    )
+    document.querySelector('#order').classList.add("dis-button");
+  }else {
+  setTimeout(function(){document.querySelector('#order').textContent = 'Ordered !'}, 1000)
+  document.querySelector('#order').classList.add("dis-button");
+  location.href="#"
+  Swal.fire(
+    'Your Product in On The Way!',
+    `Successfully Ordered ${Products[product_number].brand} ${Products[product_number].name} !`,
+    'success'
+  )  
+  setTimeout(function(){
+    location.href="https://betty.cf"
+  },7000)
+}}
+function LoadProducts(){
+	buildTable(Products)
+	function buildTable(data){
+		var table = document.querySelector('#new-drop')
+
+		for (var i = 0; i < data.length; i++){
+            var row = `<a href="${data[i].link}" class="card ${data[i].available}">
+                            <img id='${data[i].id}' src='${data[i].img}' style='${data[i].style}'">
+                            <h6>${data[i].brand}</h6>
+                            <h4>${data[i].name}</h4>
+                            <h5>${data[i].price}</h5>
+                       </a>`
+			table.innerHTML += row
+		}
+	}
+}
+function Shop(){
+	buildTable(Products)
+	function buildTable(data){
+		var table = document.querySelector('#shop-all')
+
+		for (var i = 1; i < data.length; i++){
+            var row = `<a href="${data[i].link}" class="card ${data[i].available}">
+                            <img id="${data[i].id}" src='${data[i].img}' style='${data[i].style}'">
+                            <h6>${data[i].brand}</h6>
+                            <h4>${data[i].name}</h4>
+                            <h5>${data[i].price}</h5>
+                       </a>`
+			table.innerHTML += row
+		}
+	}
+}
+function promcode(){
+    e = document.querySelector('#promo-code').value
+    if (e == code[0].code){
+        location.href="#thumb1"
+        document.querySelector('#Pprice').innerHTML = `${Products[product_number].price} (-${code[0].moins})`
+    }
+}
