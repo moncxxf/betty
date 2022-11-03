c4197c566089222685df97813042252903fb4d56
moncef <98907255+moncxxf@users.noreply.github.com>
2022-11-03 22:32:58 +0100

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -10,13 +10,13 @@ const code = [
     }
 ]
 const Products = [
-//     {
-//         'brand':'Destin',
-//         'name':'Ensemble',
-//         'price':'4700 DA',
-//         'thumb':'https://media.boohoo.com/i/boohoo/fzz51311_white_xl/f%C3%A9minin-white-jogging-oversize-basique/?w=900&qlt=default&fmt.jp2.qlt=70&fmt=auto&sm=fit',
-//         'img':'https://scontent.cdninstagram.com/v/t1.15752-9/313042209_825086422067167_6908234697615776493_n.jpg?stp=dst-jpg_p526x296&_nc_cat=108&ccb=1-7&_nc_sid=5a057b&_nc_ohc=eJ__a4NAdAkAX8WVDMb&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AdQL8bsjCp-_gL51jw-3UFJI93p6hwCj8nkBiNEzoRJK4Q&oe=638A37A7',
-//         'link':'products/ensemble.html',
-//         'available':'yes'
-//     }
+    {
+        'brand':'Destin',
+        'name':'Ensemble',
+        'price':'4700 DA',
+        'thumb':'https://media.boohoo.com/i/boohoo/fzz51311_white_xl/f%C3%A9minin-white-jogging-oversize-basique/?w=900&qlt=default&fmt.jp2.qlt=70&fmt=auto&sm=fit',
+        'img':'https://scontent.cdninstagram.com/v/t1.15752-9/313042209_825086422067167_6908234697615776493_n.jpg?stp=dst-jpg_p526x296&_nc_cat=108&ccb=1-7&_nc_sid=5a057b&_nc_ohc=eJ__a4NAdAkAX8WVDMb&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AdQL8bsjCp-_gL51jw-3UFJI93p6hwCj8nkBiNEzoRJK4Q&oe=638A37A7',
+        'link':'products/ensemble.html',
+        'available':'yes'
+    }
 ]
