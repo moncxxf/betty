79b2caa225fd64e659b02e88e92b73f7ce5f9d41
moncef <98907255+moncxxf@users.noreply.github.com>
2022-11-17 13:01:26 +0100

    Add files via upload

--- assets/data.js
+++ assets/data.js
@@ -1,8 +1,8 @@
 const FooterMessage = "Destin. Simple ting <br>Brand by <a style='color:red;' href='https://instagram.com/ichemc'>ichemc</a><br> Website Developed & Designed by <a style='color:red;' href='https://instagram.com/moncxf'>moncef</a>";
 const code = [
     {
-        'code':'maissa30',
-        'moins':'200DA'
+        'code':'Maissa30',
+        'moins':'300DA'
     },
     {
         'code':'Maissa30',
@@ -12,17 +12,17 @@ const code = [
 const Products = [
     {
         'brand':'Destin',
-        'name':'Ensemble White',
-        'price':'4700 DA',
+        'name':'Tracksuit White',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/white man back.jpg',
         'img':'https://destin.gq/assets/imgs/white man front.jpg',
         'link':'products/White.html',
-        'available':'yes'
+        'available':'sold'
     },
     {
         'brand':'Destin',
-        'name':'Ensemble Black',
-        'price':'4700 DA',
+        'name':'Tracksuit Black',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/black gyal back2.jpg',
         'img':'https://destin.gq/assets/imgs/black gyal front.jpg',
         'link':'products/Black.html',
@@ -30,17 +30,17 @@ const Products = [
     },
     {
         'brand':'Destin',
-        'name':'Ensemble Beige',
-        'price':'4700 DA',
+        'name':'Tracksuit Beige',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/beige back.jpg',
         'img':'https://destin.gq/assets/imgs/beige front.jpg',
         'link':'products/Beige.html',
-        'available':'yes'
+        'available':'sold'
     },
     {
         'brand':'Destin',
-        'name':'Ensemble Blue',
-        'price':'4700 DA',
+        'name':'Tracksuit Blue',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/blue back.jpg',
         'img':'https://destin.gq/assets/imgs/blue front.jpg',
         'link':'products/Blue.html',
@@ -48,8 +48,8 @@ const Products = [
     },
     {
         'brand':'Destin',
-        'name':'Ensemble Brown',
-        'price':'4700 DA',
+        'name':'Tracksuit Brown',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/brown back.jpg',
         'img':'https://destin.gq/assets/imgs/brown front.jpg',
         'link':'products/Brown.html',
@@ -57,8 +57,8 @@ const Products = [
     },
     {
         'brand':'Destin',
-        'name':'Ensemble Gray',
-        'price':'4700 DA',
+        'name':'Tracksuit Gray',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/gray gyal back.jpg',
         'img':'https://destin.gq/assets/imgs/gray gyal front1.jpg',
         'link':'products/Gray.html',
@@ -66,8 +66,8 @@ const Products = [
     },
     {
         'brand':'Destin',
-        'name':'Ensemble Green',
-        'price':'4700 DA',
+        'name':'Tracksuit Green',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/green gyal back.jpg',
         'img':'https://destin.gq/assets/imgs/green gyal front.jpg',
         'link':'products/Green.html',
@@ -75,8 +75,8 @@ const Products = [
     },
     {
         'brand':'Destin',
-        'name':'Ensemble Red',
-        'price':'4700 DA',
+        'name':'Tracksuit Garnet',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/red man back.jpg',
         'img':'https://destin.gq/assets/imgs/red man front.jpg',
         'link':'products/Red.html',
@@ -84,8 +84,8 @@ const Products = [
     },
     {
         'brand':'Destin',
-        'name':'Ensemble SkyBlue',
-        'price':'4700 DA',
+        'name':'Tracksuit SkyBlue',
+        'price':'4800 DA',
         'thumb':'https://destin.gq/assets/imgs/skyblue back.jpg',
         'img':'https://destin.gq/assets/imgs/skyblue front.jpg',
         'link':'products/SkyBlue.html',
